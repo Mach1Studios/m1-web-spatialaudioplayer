edbf4840ec27f38dacbcba8ea5d6cae42774eea0
Dylan Marcus <dylan@mach1studios.com>
2021-03-16 21:07:28 -0600

    updated demo audio file source paths

--- download-audiofiles.sh
+++ download-audiofiles.sh
@@ -17,21 +17,21 @@ fi
 mkdir -p audio/m1spatial
 
 cd audio/m1spatial
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T1.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T2.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T3.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T4.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B5.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B6.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B7.ogg
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B8.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T1.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T2.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T3.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T4.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B5.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B6.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B7.ogg
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B8.ogg
 
 # sound files for iOS and iPadOS
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T1.mp3
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T2.mp3
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T3.mp3
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/T4.mp3
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B5.mp3
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B6.mp3
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B7.mp3
-wget http://dev.mach1.xyz/mach1-sdk-sample-audio/web/m1spatial/B8.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T1.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T2.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T3.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/T4.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B5.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B6.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B7.mp3
+wget http://dev.mach1.tech/mach1-sdk-sample-audio/web/m1spatial/B8.mp3
